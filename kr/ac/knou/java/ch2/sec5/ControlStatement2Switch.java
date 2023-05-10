1c3a0877bddb912659a5cb87beea632505f2195f
DaeHyeonHong <fholng@outlook.com>
2023-05-11 01:18:39 +0900

    switch

--- kr/ac/knou/java/ch2/sec5/ControlStatement2Switch.java
+++ kr/ac/knou/java/ch2/sec5/ControlStatement2Switch.java
@@ -1,12 +1,12 @@
 package kr.ac.knou.java.ch2.sec5;
 
+import java.util.Random;
+
 public class ControlStatement2Switch {
     public static void main(String[] args) {
-        double v = Math.random() * 3;
-        double v1 = v * 10;
-        int n = (int) v1;
-        switch (n) {
-            case 10 -> System.out.println(n + "입니다.");
+        final int result = (new Random().nextInt(9) + 1) * 10;
+        switch (result) {
+            case 10 -> System.out.println(result + "입니다.");
             case 20, 30 -> System.out.println("20이거나 30입니다.");
             default -> System.out.println("모르겠습니다.");
         }
