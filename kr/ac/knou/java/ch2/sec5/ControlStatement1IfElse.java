4e010bd852786499cac12a993e20a5710120a396
DaeHyeonHong <fholng@outlook.com>
2023-05-11 01:11:13 +0900

    ifElse

---
+++ kr/ac/knou/java/ch2/sec5/ControlStatement1IfElse.java
@@ -0,0 +1,13 @@
+package kr.ac.knou.java.ch2.sec5;
+
+public class ControlStatement1IfElse {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        if (a == 1)
+            if (b == 2)
+                System.out.println("a was 2 and b was 2.");
+            else
+                System.out.println("a was not 1.");
+    }
+}
