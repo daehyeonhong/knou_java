e8613afce6bfa3e4445f97f6232b62e4d6e2529e
DaeHyeonHong <fholng@outlook.com>
2023-05-11 00:46:14 +0900

    CommandInputTests

---
+++ kr/ac/knou/java/ch2/sec2/CommandInputTest.java
@@ -0,0 +1,9 @@
+package kr.ac.knou.java.ch2.sec2;
+
+public class CommandInputTest {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        int i = Integer.parseInt(args[1]);
+        System.out.println(i);
+    }
+}
